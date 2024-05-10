9c7331ae1746b7bddb8357c19c1ad90d47266024
amar-shrestha <135716053+amar-shrestha@users.noreply.github.com>
2024-05-10 12:33:46 +1000

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-// JavaScript for fading testimonials
-let currentTestimonial = 0;
-const testimonials = document.querySelectorAll('.testimonial');
-setInterval(() => {
-    testimonials[currentTestimonial].style.opacity = 0; // Fade out
-    currentTestimonial = (currentTestimonial + 1) % testimonials.length;
-    testimonials[currentTestimonial].style.opacity = 1; // Fade in
-}, 3000);
